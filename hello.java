47e2ad8b1f17fe29dcd94517c3ec44155b6eb3a3
brainnoopen <793536301@qq.com>
2018-10-04 15:33:48 +1000

    solve the confliction

--- hello.java
+++ hello.java
@@ -1,10 +1,6 @@
 class hello{
 	public static void main(String[] args){
-<<<<<<< HEAD
-		int a =18;
-=======
-		int a =12;
->>>>>>> 5f567072f2b436e25f53a307276d3ae8a432004d
+		int a =20;
 		system.out.print("hello");
 		system.out.print("change the code");
 	}
